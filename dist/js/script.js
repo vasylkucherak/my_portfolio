f531c38e77a6fcce4b5404c4675eaab9fe5a578d
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-01 18:51:27 +0200

    v.0.1.0 (added menu component)

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      menuClose = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
