2850aaf01b048edc65dafd6d690ae073dc1b77d8
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-03 23:11:05 +0200

    v.0.4.5 (added transition on menu and buttons)

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,17 @@ window.addEventListener('DOMContentLoaded', () => {
     // Activate Menu
     const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      menuClose = document.querySelector('.menu__close');
+      menuClose = document.querySelector('.menu__close'),
+      menuLink = document.querySelectorAll('.menu__link');
 
     hamburger.addEventListener('click', () => {
         menu.classList.add('active');
+    });
+
+    menuLink.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active');
+        });
     });
 
     menuClose.addEventListener('click', () => {
