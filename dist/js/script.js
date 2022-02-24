0997afc0c984d0d5d43e4cd7c651173fb12118cf
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-24 12:21:14 +0200

    v.0.8.3 (working on the appearance of the site)

--- dist/js/script.js
+++ dist/js/script.js
@@ -253,3 +253,36 @@ dots.forEach(dot => {
         dots[slideIndex-1].style.opacity = 1;
     });
 });
+
+// Scroll On Links
+let links = document.querySelectorAll('[href^="#"]'),
+    speed = 0.3;
+
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        let widthTop = document.documentElement.scrollTop,
+            hash = this.hash,
+            toBlock = document.querySelector(hash).getBoundingClientRect().top,
+            start = null;
+
+        requestAnimationFrame(step);
+
+        function step(time) {
+            if (start === null) {
+                start = time;
+            }
+
+            let progress = time - start,
+                r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock));
+                document.documentElement.scrollTo(0, r);
+
+            if (r != widthTop + toBlock) {
+                requestAnimationFrame(step);
+            } else {
+                location.hash = hash;
+            }
+        }
+    });
+});
