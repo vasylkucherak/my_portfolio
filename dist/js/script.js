cdca59168ce3806779fa1d307004e80a643ed054
vasylkucherak <vasil.kucherak@gmail.com>
2022-04-15 16:07:04 +0300

    v.0.9.1(added progress bar and new skill on the site)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,13 @@
 'use strict'
+// Proggres Bar
+window.onscroll = function() {proggresBar()};
+
+function proggresBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.height = scrolled + "%";
+}
 
 // AnimationByScroll
 const upBtn = document.querySelector('.up_btn');
